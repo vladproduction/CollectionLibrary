c6ad1589f2d3b8f99920527cebe74e75695567fd
VladBogda <vladbogdantsev@gmail.com>
2023-04-25 11:35:24 +0300

    app03 linkedList(remove, toString)

--- src/main/java/com/app03/linkedList/Main.java
+++ src/main/java/com/app03/linkedList/Main.java
@@ -3,30 +3,66 @@ package com.app03.linkedList;
 public class Main {
     public static void main(String[] args) {
         MyLinkedList myLinkedList = new MyLinkedList();
+//        myLinkedList.add("a");
+//        myLinkedList.add("b");
+//        myLinkedList.add("c");
+//        boolean isEmpty = myLinkedList.isEmpty();
+//        System.out.println(isEmpty);
+//        int sizeRez = myLinkedList.size();
+//        System.out.println(sizeRez);
+//        System.out.println(myLinkedList.get(0));
+//        System.out.println(myLinkedList.get(1));
+//        System.out.println(myLinkedList.get(2));
+//        myLinkedList.set(0,"AAA");
+//        sizeRez = myLinkedList.size();
+//        System.out.println(sizeRez);
+//        System.out.println(myLinkedList.get(0));
+//        myLinkedList.set(2,"CCC");
+//        Object oldValue = myLinkedList.set(1,"BBB");
+//        System.out.println(myLinkedList.get(1));
+//        System.out.println(myLinkedList.get(2));
+//        sizeRez = myLinkedList.size();
+//        System.out.println(sizeRez);
+//        System.out.println(oldValue);
+//        myLinkedList.clear();
+
         myLinkedList.add("a");
         myLinkedList.add("b");
         myLinkedList.add("c");
-        boolean isEmpty = myLinkedList.isEmpty();
-        System.out.println(isEmpty);
+        myLinkedList.add("d");
         int sizeRez = myLinkedList.size();
         System.out.println(sizeRez);
-        System.out.println(myLinkedList.get(0));
-        System.out.println(myLinkedList.get(1));
-        System.out.println(myLinkedList.get(2));
-        myLinkedList.set(0,"AAA");
+        myLinkedList.remove(3);
         sizeRez = myLinkedList.size();
         System.out.println(sizeRez);
-        System.out.println(myLinkedList.get(0));
-        myLinkedList.set(2,"CCC");
-        Object oldValue = myLinkedList.set(1,"BBB");
-        System.out.println(myLinkedList.get(1));
-        System.out.println(myLinkedList.get(2));
+        System.out.println(myLinkedList);
+        System.out.println("-------------");
+        myLinkedList.remove(0);
         sizeRez = myLinkedList.size();
         System.out.println(sizeRez);
-        System.out.println(oldValue);
+        System.out.println(myLinkedList);
+        System.out.println("------remove middle:--------");
+
         myLinkedList.clear();
+
+        myLinkedList.add("a");
+        myLinkedList.add("b");
+        myLinkedList.add("c");
+        myLinkedList.add("d");
         sizeRez = myLinkedList.size();
         System.out.println(sizeRez);
+        System.out.println(myLinkedList);
+        myLinkedList.remove(2);
+        sizeRez = myLinkedList.size();
+        System.out.println(sizeRez);
+        System.out.println(myLinkedList);
+        myLinkedList.remove(1);
+        sizeRez = myLinkedList.size();
+        System.out.println(sizeRez);
+        System.out.println(myLinkedList);
+
+
+
 
 
 
--- src/main/java/com/app03/linkedList/MyLinkedList.java
+++ src/main/java/com/app03/linkedList/MyLinkedList.java
@@ -75,13 +75,51 @@ public class MyLinkedList implements MyList {
     @Override
     public void remove(int index) {
         int step = 0;
-        Node tmp = root;
-        while (tmp.getNext()!=null){
-            if(step==index){
-                break;
+        if(index==0){
+            root=root.getNext();
+            size--;
+            return;
+        }
+            if(index==size-1){
+                Node tmp = root;
+                while (tmp.getNext()!=null){
+                    if(step==index-1){
+                        break;
+                    }
+                    tmp = tmp.getNext();
+                    step++;
+                }
+                tmp.setNext(null);
+            }else{
+                Node tmp = root;
+                while (tmp.getNext()!=null){
+                    if(step==index-1){
+                        break;
+                    }
+                    tmp = tmp.getNext();
+                    step++;
+                }
+                Node toDeleteNode = tmp.getNext();
+                Node nextAfterDeleted = toDeleteNode.getNext();
+                tmp.setNext(nextAfterDeleted);
             }
+        size--;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        Node tmp = root;
+        while (tmp!=null){
+            Object value = tmp.getValue();
+            sb.append(value);
+            sb.append(",");
             tmp = tmp.getNext();
         }
-
+        int comaPosition = sb.length()-1;
+        sb.deleteCharAt(comaPosition);
+        sb.append("]");
+        return sb.toString();
     }
 }
