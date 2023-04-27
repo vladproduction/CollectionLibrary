41beae77856d7dd512988230986fc4b3db22669c
VladBogda <vladbogdantsev@gmail.com>
2023-04-27 10:30:47 +0300

    upload project (add)

---
+++ src/test/java/App01_Array/BagImplTest.java
@@ -0,0 +1,27 @@
+package App01_Array;
+
+import com.Bag;
+import com.app01_Array.BagImpl;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BagImplTest {
+
+    @Test
+    public void apiTest(){
+        Bag data = new BagImpl();
+        Assert.assertTrue(data.isEmpty());
+        Assert.assertTrue(data.size()==0);
+        data.add("a");
+        data.add("b");
+        data.add("c");
+        Assert.assertFalse(data.isEmpty());
+        Assert.assertTrue(data.size()==3);
+        data.add("d");
+        data.add("e");
+        Assert.assertFalse(data.size()==3);
+        Assert.assertTrue(data.size()==5);
+
+
+    }
+}
