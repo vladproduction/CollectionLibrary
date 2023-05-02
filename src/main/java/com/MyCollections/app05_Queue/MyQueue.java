d2095a9a3fd4a7084733c7833d4492805a970299
VladBogda <vladbogdantsev@gmail.com>
2023-05-02 14:39:45 +0300

    MyQueue (pack) add + test(api, hash_equals);

---
+++ src/main/java/com/MyCollections/app05_Queue/MyQueue.java
@@ -0,0 +1,8 @@
+package com.MyCollections.app05_Queue;
+
+public interface MyQueue {
+    public int size();
+    public boolean isEmpty();
+    public void enQueue(Object o);
+    public Object deQueue();
+}
