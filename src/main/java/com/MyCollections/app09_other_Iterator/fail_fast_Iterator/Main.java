a3b249c29d6d44eac727354bb6a5b918c2bca925
VladBogda <vladbogdantsev@gmail.com>
2023-05-22 11:59:39 +0300

    app09_other_Iterator.list_Iterator (pack) add;

---
+++ src/main/java/com/MyCollections/app09_other_Iterator/fail_fast_Iterator/Main.java
@@ -0,0 +1,35 @@
+package com.MyCollections.app09_other_Iterator.fail_fast_Iterator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> listString = new ArrayList();
+        listString.add("a");
+        listString.add("b");
+        listString.add("c");
+        listString.add("d");
+
+        System.out.println("ListIterator<String>: ");
+        ListIterator<String> listIterator = listString.listIterator();
+        while (listIterator.hasNext()){
+            String item = listIterator.next();
+            if ("c".equals(item)){
+                listIterator.remove();
+            }
+            System.out.println(item);
+        }
+        System.out.println(listString); //now we se: deleted "c"
+
+//        System.out.println("-------------------");
+//        for (String item: listString) {
+//            if("c".equals(item)){
+//                listString.remove(2); //it`s ConcurrentModificationException
+//                //versions are not same; iteratorVersion= 4 ;version=5
+//                System.out.println(item);
+//            }
+//        }
+    }
+}
---
+++ src/main/java/com/MyCollections/app09_other_Iterator/fail_fast_Iterator/Main2.java
@@ -0,0 +1,36 @@
+package com.MyCollections.app09_other_Iterator.fail_fast_Iterator;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+public class Main2 {
+    public static void main(String[] args) {
+        List<String> listString = new ArrayList();
+        listString.add("a");
+        listString.add("b");
+        listString.add("c");
+        listString.add("d");
+
+        System.out.println("ListIterator<String>: ");
+        Iterator<String> listIterator = listString.iterator();
+        while (listIterator.hasNext()){
+            String item = listIterator.next();
+            if ("c".equals(item)){
+                listString.remove("c");
+            }
+            System.out.println(item);
+        }
+        System.out.println(listString); //now we se: deleted "c"
+
+//        System.out.println("-------------------");
+//        for (String item: listString) {
+//            if("c".equals(item)){
+//                listString.remove(2); //it`s ConcurrentModificationException
+//                //versions are not same; iteratorVersion= 4 ;version=5
+//                System.out.println(item);
+//            }
+//        }
+    }
+}
